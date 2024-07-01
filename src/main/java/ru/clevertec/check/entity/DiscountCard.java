4165a825fb9c100a8f158eec82df11ba54c4e33a
Ivan Sapozhnikov <ivan101454@gmail.com>
2024-07-02 01:00:34 +0300

    start project

---
+++ src/main/java/ru/clevertec/check/entity/DiscountCard.java
@@ -0,0 +1,8 @@
+package ru.clevertec.check.entity;
+
+public class DiscountCard {
+    private Long idDiscountCard;
+    private int numberOfDiscountCard;
+    private short discountAmount;
+
+}
---
+++ src/main/java/ru/clevertec/check/entity/Product.java
@@ -0,0 +1,11 @@
+package ru.clevertec.check.entity;
+
+import java.math.BigDecimal;
+
+public class Product {
+    private Long productId;
+    private String productDescription;
+    private BigDecimal productPrice;
+    private int quantityInStock;
+    private boolean isWholesaleProduct;
+}
